348a0afb9f726354ff4dc292df65b8701ba5d4fe
kunal <13kunalthakur@gmail.com>
2023-07-07 10:18:49 +0530

    cycle detection in undirected graph using bfs

--- graph/cycleDetectionInUndirectedGraph.cpp
+++ graph/cycleDetectionInUndirectedGraph.cpp
@@ -5,10 +5,10 @@
 #include<queue>
 using namespace std;
 
-void preparedList(unordered_map<int , list<int>> &adjlist, vector<pair<int, int>> &edges){
+void preparedList(unordered_map<int , list<int>> &adjlist, vector<vector<int>> &edges){
     for(int i = 0; i < edges.size(); i++) {
-        int u = edges[i].first;
-        int v = edges[i].second;
+        int u = edges[i][0];
+        int v = edges[i][1];
 
         adjlist[u].push_back(v);
         adjlist[v].push_back(u);
@@ -16,7 +16,7 @@ void preparedList(unordered_map<int , list<int>> &adjlist, vector<pair<int, int>
 }
 
 bool isCycle(unordered_map<int , list<int>> &adjList, unordered_map<int, bool> &visited, int node){
-    unordered_map<int, bool> parent;
+    unordered_map<int, int> parent;
 
     parent[node] = -1;
     visited[node] = true;
@@ -29,7 +29,7 @@ bool isCycle(unordered_map<int , list<int>> &adjList, unordered_map<int, bool> &
         q.pop();
         
         for(auto i : adjList[frontNode]) {
-            if(visited[i] && i != parent[node]){
+            if(visited[i] == true && i != parent[frontNode]){
                 return true;
             }
             else if(!visited[i]){
@@ -43,25 +43,19 @@ bool isCycle(unordered_map<int , list<int>> &adjList, unordered_map<int, bool> &
     return false;
 }
 
-string cycleDetection(int v, int e, vector<pair<int, int>> edges){
+string cycleDetection(vector<vector<int>>& edges, int n, int m){
     unordered_map<int , list<int>> adjList;
     unordered_map<int, bool> visited;
 
     preparedList(adjList, edges);
 
-    for(int i = 0; i < v; i++) {
+    for(int i = 0; i < n; i++) {
         if(!visited[i]) {
             bool ans = isCycle(adjList, visited, i);
-            if(ans) return "yes";
+            if(ans == 1) return "Yes";
         }
     }
 
     return "No";
 }
 
-int main() {
-
-    // printGraph();
-
-    return 0;
-}
