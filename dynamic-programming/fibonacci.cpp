c973b5de152d191c2b7131fc7dfb6cc408c88d72
kunal <13kunalthakur@gmail.com>
2023-07-15 11:25:26 +0530

    dynamic programming

---
+++ dynamic-programming/fibonacci.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+// DP top down approach****************************************************************************************************************
+int fibonacci(int n, vector<int> &dp){
+    if(n <= 1){
+        return n;
+    }
+    if(dp[n] != -1) return dp[n];
+    
+    dp[n] =  fibonacci(n-1, dp) + fibonacci(n-2, dp);
+    return dp[n];
+}
+
+
+int main(){
+    int n = 4;
+    vector<int> dp(n+1, -1);
+    cout << fibonacci(n,dp);
+    return 0;
+}
