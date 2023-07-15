4b4f18969c154e27f0a8c5d5c15d2c6725b31ea9
kunal <13kunalthakur@gmail.com>
2023-07-15 11:27:01 +0530

    find fibonacci number by bottom down approach

--- dynamic-programming/fibonacci.cpp
+++ dynamic-programming/fibonacci.cpp
@@ -4,19 +4,32 @@
 using namespace std;
 
 // DP top down approach****************************************************************************************************************
-int fibonacci(int n, vector<int> &dp){
-    if(n <= 1){
-        return n;
-    }
-    if(dp[n] != -1) return dp[n];
+// int fibonacci(int n, vector<int> &dp){
+//     if(n <= 1){
+//         return n;
+//     }
+//     if(dp[n] != -1) return dp[n];
     
-    dp[n] =  fibonacci(n-1, dp) + fibonacci(n-2, dp);
+//     dp[n] =  fibonacci(n-1, dp) + fibonacci(n-2, dp);
+//     return dp[n];
+// }
+
+// DP bottom up approach****************************************************************************************************************
+int fibonacci(int n){
+    
+    vector<int> dp(n+1);
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for(int i = 2; i <= n; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
     return dp[n];
 }
 
 int main(){
     int n = 4;
     vector<int> dp(n+1, -1);
-    cout << fibonacci(n,dp);
+    cout << fibonacci(n);
     return 0;
 }
