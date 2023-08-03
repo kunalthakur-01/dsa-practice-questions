1e89978bc9f71ba614fcc6cdfc00e470642c1a8e
kunal <13kunalthakur@gmail.com>
2023-08-03 10:15:08 +0530

    bottom up approach

--- dynamic-programming/frogJump.cpp
+++ dynamic-programming/frogJump.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-// Recursive solution
+// Recursive solution********************************************************************************************************************
 // int frogJump(int i, vector<int> heights) {
 //     if(i == 0) return 0;
 
@@ -21,22 +21,47 @@ using namespace std;
 //     frogJump(heights.size()-1, heights);
 // }
 
-// Top down solution
-int frogJump(int i, vector<int> heights, vector<int> &dp) {
-    if(i == 0) return 0;
 
-    if(dp[i] != -1) return dp[i];
+// Top down solution*********************************************************************************************************************
+// int frogJump(int i, vector<int> heights, vector<int> &dp) {
+//     if(i == 0) return 0;
 
-    int left = frogJump(i-1, heights, dp) + abs(heights[i] - heights[i-1]);
-    int right = INT_MAX;
-    if(i > 1) int right = frogJump(i-2, heights, dp) + abs(heights[i] - heights[i-2]);
+//     if(dp[i] != -1) return dp[i];
 
-    return dp[i] = min(left, right);
+//     int left = frogJump(i-1, heights, dp) + abs(heights[i] - heights[i-1]);
+//     int right = INT_MAX;
+//     if(i > 1) int right = frogJump(i-2, heights, dp) + abs(heights[i] - heights[i-2]);
+
+//     return dp[i] = min(left, right);
+// }
+
+// int main() {
+//     vector<int> heights = {20,30,40,10};
+//     int n = heights.size();
+//     vector<int> dp(n+1, -1);
+//     frogJump(n-1, heights, dp);
+// }
+
+// Bottom up approach*******************************************************************************************************************
+int frogJump(int n, vector<int> heights) {
+    vector<int> dp;
+
+    dp[0] = 0;
+
+    for(int i = 0; i < n; i++) {
+        int firstStep = dp[i - 1] + abs(heights[i] - heights[i-1]);
+        int secondStep =  INT_MAX;
+        if(i > 1) secondStep = dp[i - 2] + abs(heights[i] - heights[i-2]);
+
+        dp[i] = min(firstStep, secondStep);
+    }
+
+    return dp[n - 1];
 }
 
 int main() {
     vector<int> heights = {20,30,40,10};
     int n = heights.size();
-    vector<int> dp(n+1, -1);
-    frogJump(n-1, heights, dp);
+    frogJump(n, heights);
 }
+
